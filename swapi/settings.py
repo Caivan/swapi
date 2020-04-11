17f0749464a8729c35f956889029da2677482363
Caivan <caivan2@gmail.com>
2020-04-11 14:37:37 -0500

    os.path.join(BASE_DIR, 'staticfiles')

--- swapi/settings.py
+++ swapi/settings.py
@@ -71,9 +71,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DISABLE_COLLECTSTATIC=1
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
