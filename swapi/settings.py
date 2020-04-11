a871683bca04f3752a52e7f3210a98b920745542
Caivan <caivan2@gmail.com>
2020-04-11 16:10:00 -0500

    Add middleware 'whitenoise.middleware.WhiteNoiseMiddleware',

--- swapi/settings.py
+++ swapi/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 # Configure Django App for Heroku.
