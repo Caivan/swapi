f04b200e24d3702a4cbde243ca873a7866cbabe3
Caivan <caivan2@gmail.com>
2020-04-11 14:33:20 -0500

    set DISABLE_COLLECTSTATIC=1

--- swapi/settings.py
+++ swapi/settings.py
@@ -71,6 +71,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
+DISABLE_COLLECTSTATIC=1
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
